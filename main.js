371daa90e9f185b33143846ed7e8a0ceba9ba58e
hardikWottaweb <hardik.wottaweb@gmail.com>
2023-01-17 17:13:11 +0530

    initial

---
+++ main.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body'),
+sidebar = body.querySelector('nav'),
+toggle = body.querySelector(".toggle"),
+searchBtn = body.querySelector(".search-box"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+nav_link = body.querySelector(".nav-link")
+
+
+toggle.addEventListener("click" , () =>{
+sidebar.classList.toggle("close");
+
+})
+
+
+
+modeSwitch.addEventListener("click" , () =>{
+body.classList.toggle("dark");
+
+if(body.classList.contains("dark")){
+  modeText.innerText = "Light mode";
+}else{
+  modeText.innerText = "Dark mode";
+  
+}
+});
+
+63.00
